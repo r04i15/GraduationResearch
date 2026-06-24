f5841243b46cbca447c1555941be3b9beadf7fca
r04i15 <kerokero.sh@mineo.jp>
2026-06-24 12:00:19 +0900

    update:main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import math
 
 # Face Mesh初期化
 mp_face_mesh = mp.solutions.face_mesh
@@ -14,6 +15,8 @@ face_mesh = mp_face_mesh.FaceMesh(
 
 # カメラ起動
 cap = cv2.VideoCapture(0)
+
+previous_direction = ""
 
 while True:
     success, img = cap.read()
@@ -39,6 +42,28 @@ while True:
             nose = face_landmarks.landmark[1]
             left_eye = face_landmarks.landmark[33]
             right_eye = face_landmarks.landmark[263]
+            left_cheek = face_landmarks.landmark[234]
+            right_cheek = face_landmarks.landmark[454]
+
+            x1 = int(left_cheek.x * w)
+            y1 = int(left_cheek.y * h)
+
+            x2 = int(right_cheek.x * w)
+            y2 = int(right_cheek.y * h)
+
+            face_width = math.sqrt(
+                (x2 - x1) ** 2 +
+                (y2 - y1) ** 2
+            )
+                        
+            REFERENCE_DISTANCE = 50
+            REFERENCE_FACE_WIDTH = 250
+
+            distance_cm = (
+                REFERENCE_FACE_WIDTH *
+                REFERENCE_DISTANCE /
+                face_width
+            )
 
             nose_x = nose.x
             left_x = left_eye.x
@@ -47,10 +72,10 @@ while True:
             center = (left_x + right_x) / 2
 
             if nose_x < center - 0.02:
-                direction = "RIGHT"
+                direction = "LEFT"
 
             elif nose_x > center + 0.02:
-                direction = "LEFT"
+                direction = "RIGHT"
 
             else:
                 direction = "CENTER"
@@ -64,7 +89,19 @@ while True:
                 (0, 255, 0),
                 2
             )
-            print(direction)
+            if direction != previous_direction:
+                print(direction)
+                previous_direction = direction
+
+            cv2.putText(
+                img,
+                f"Distance: {distance_cm:.1f} cm",
+                (50, 100),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (255, 0, 0),
+                2
+        )
 
             # 全ランドマークに番号表示
             for idx, landmark in enumerate(face_landmarks.landmark):
